9c0f94b10a8ec3edb0a67f53638d554f7064347e
v-nakon <nakonechnyvlad@gmail.com>
2020-03-24 15:28:24 +0200

    added search function

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,13 +1,91 @@
-for(let i=1; i<7; i++) {
-	var date = new Date();
-	let unixtimeDate = date.setDate(date.getDate()+i);
-	moment.locale("ru");
-	console.log(moment(unixtimeDate).format('DD'));
-	console.log(moment(unixtimeDate).format('dd'));
-	createSearchDays(moment(unixtimeDate).format('dd'), moment(unixtimeDate).format('DD'));
+createBlockDate();
+getCategories();
+getCities("city");
+
+var searchName = document.querySelector("#event_name");
+var searchLocation = document.querySelector("#city");
+var modalNotFound = document.querySelector(".container_modal_notfound");
+var closeModalNotfound = document.querySelector(".close_modal_notfound");
+
+closeModalNotfound.addEventListener("click", function() {
+	modalNotFound.style.display = "none";
+	document.location.href = "/";
+  });
+
+let btnSearch = document.querySelector(".btn_search");
+btnSearch.addEventListener('click',() => searchEvent("event_name", "city"));
+
+function viewEvent(id) {
+	console.log("ID", id);
+	console.log(window.location.href);
+	let urlEvent = window.location.href + "event-page.html?id=" + id;
+	window.open(urlEvent);
 }
 
-function createSearchDays(dayName, dayCount) {
+function createEventCard(objItem) {
+	let eventCardElements = 
+	` <div class="event_card_content">
+	<div class="block_event_img">
+		<img src="https://eventafisha.com/storage/` + objItem.images + `" alt="` + sliceText(objItem.title, 20) + `" class="event_card_img">
+	</div>
+
+	<div class="event_card_title">` + sliceText(objItem.title, 60) + `</div>
+	<div class="event_card_desc">` + sliceText(objItem.desc, 120) + `</div>
+	<div class="card_icon_title">
+		<svg xmlns="http://www.w3.org/2000/svg" class="event_card_icon" viewBox="0 0 24 24"><path d="M17 3v-2c0-.552.447-1 1-1s1 .448 1 1v2c0 .552-.447 1-1 1s-1-.448-1-1zm-12 1c.553 0 1-.448 1-1v-2c0-.552-.447-1-1-1-.553 0-1 .448-1 1v2c0 .552.447 1 1 1zm13 13v-3h-1v4h3v-1h-2zm-5 .5c0 2.481 2.019 4.5 4.5 4.5s4.5-2.019 4.5-4.5-2.019-4.5-4.5-4.5-4.5 2.019-4.5 4.5zm11 0c0 3.59-2.91 6.5-6.5 6.5s-6.5-2.91-6.5-6.5 2.91-6.5 6.5-6.5 6.5 2.91 6.5 6.5zm-14.237 3.5h-7.763v-13h19v1.763c.727.33 1.399.757 2 1.268v-9.031h-3v1c0 1.316-1.278 2.339-2.658 1.894-.831-.268-1.342-1.111-1.342-1.984v-.91h-9v1c0 1.316-1.278 2.339-2.658 1.894-.831-.268-1.342-1.111-1.342-1.984v-.91h-3v21h11.031c-.511-.601-.938-1.273-1.268-2z"/></svg>
+		<div class="card_title">` + new Date(objItem.start_date).toLocaleDateString() + `</div>
+	</div>
+	<div class="card_icon_title">
+		<svg xmlns="http://www.w3.org/2000/svg" class="event_card_icon" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
+		<div class="card_title">` + objItem.address + `</div>
+	</div>
+	<div class="event_card_cat">` + objItem.category.title + `</div>
+	<input class="btn_buy_ticket" type="submit" value="Купить билет" onclick="viewEvent(`+ objItem.id +`)">
+  </div>`;
+	// console.log(test);
+
+	let listEventsElement = document.querySelector(".container_events");
+	let eventCardElement = document.createElement('li');
+	eventCardElement.className = "event_card";
+	eventCardElement.innerHTML = eventCardElements;
+	listEventsElement.append(eventCardElement);
+}
+
+function getCities(elementSelect) {
+	axios.get('https://eventafisha.com/api/v1/cities')
+	.then(function (response) {
+	  for(let item in response.data) {
+		addOptionSelect(response.data[item], elementSelect);
+	  };
+	})
+	.catch(function (error) {
+	  // handle error
+	  console.log(error);
+	})
+	.then(function () {
+	  // always executed
+	});
+};
+function addOptionSelect(item, elementSelect) {
+	let selectCategory = document.getElementById(elementSelect);
+	let option = document.createElement("option");
+	option.value = item.id;
+	option.innerHTML = item.title;
+	selectCategory.add(option);
+  }
+// START BLOCK SEARCH DATE
+function createBlockDate() {
+	for(let i=0; i<6; i++) {
+		var date = new Date();
+		let unixtimeDate = date.setDate(date.getDate()+i);
+		moment.locale("ru");
+		console.log(moment(unixtimeDate).format('DD'));
+		console.log(moment(unixtimeDate).format('dd'));
+		console.log(moment(unixtimeDate).format('DD.MM.YYYY'));
+		createSearchDays(moment(unixtimeDate).format('dd'), moment(unixtimeDate).format('DD'), moment(unixtimeDate).format('DD.MM.YYYY'));
+	};
+};
+function createSearchDays(dayName, dayCount, dateForSearch) {
 	let dayEl;
 	if(dayName==='сб' || dayName==='вс') {
 		dayEl = `
@@ -27,8 +105,93 @@ function createSearchDays(dayName, dayCount) {
 	eventCardElement.className = "item_search_date";
 	eventCardElement.innerHTML = dayEl;
 	listDays.append(eventCardElement);
+	addEventElementDate(eventCardElement, dateForSearch);
 };
+function addEventElementDate(element, searchDate) {
+	element.addEventListener("click", function(){
+		dp.clear();
+		delActiveColor("bg_chosen_day");
+		console.log("Data - ", searchDate);
+		element.classList.add("bg_chosen_day");
+		fromDateSearch = searchDate;
+		paginationAjax('#pagination', nameEventSearch, cityEventSearch, fromDateSearch, toDateSearch, categorySearch);
+		// document.querySelector(".arrow_down").classList.add("color_active_cat");
+		
+	});
+};
+var allDateEl = document.querySelector(".all_days");
+allDateEl.addEventListener("click", function() {
+	delActiveColor("bg_chosen_day");
+	allDateEl.classList.add("bg_chosen_day");
+});
+// END BLOCK SEARCH DATE
+// START categories for search
+function getCategories() {
+    axios.get('https://eventafisha.com/api/v1/categories')
+    .then(function (response) {
+      for(let item in response.data) {
+		addCatToSearch(response.data[item], ".container_category");
+      };
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+};
+function addCatToSearch(item, elementTo) {
+	// console.log(item);
+	let catElements = document.querySelector(elementTo);
+	let newCat = document.createElement("div");
+	newCat.classList.add("item_category");
+    newCat.setAttribute('category_id', item.id);
+    newCat.innerHTML = item.title;
+	catElements.appendChild(newCat);
+	addEventToElement(newCat, item.id);
+};
+function addEventToElement(element, catId) {
+	element.addEventListener("click", function(){
+		delActiveColor("color_active_cat");
+		console.log("CAT - ", catId);
+		element.classList.add("color_active_cat");
+		document.querySelector(".arrow_down").classList.add("color_active_cat");
+		categorySearch = catId;
+		paginationAjax('#pagination', nameEventSearch, cityEventSearch, fromDateSearch, toDateSearch, categorySearch);
+	});
+};
+var elCatAll = document.querySelector("#search_cat_all");
+var elCatMain1 = document.querySelector("#search_cat_main1");
+var elCatMain2 = document.querySelector("#search_cat_main2");
 
+elCatAll.addEventListener("click", function(){
+	delActiveColor("color_active_cat");
+	elCatAll.classList.add("color_active_cat");
+	categorySearch = '';
+	paginationAjax('#pagination', nameEventSearch, cityEventSearch, fromDateSearch, toDateSearch, categorySearch);
+});
+elCatMain1.addEventListener("click", function(){
+	delActiveColor("color_active_cat");
+	elCatMain1.classList.add("color_active_cat");
+	categorySearch = 4;
+	paginationAjax('#pagination', nameEventSearch, cityEventSearch, fromDateSearch, toDateSearch, categorySearch);
+});
+elCatMain2.addEventListener("click", function(){
+	delActiveColor("color_active_cat");
+	elCatMain2.classList.add("color_active_cat");
+	categorySearch = 5;
+	paginationAjax('#pagination', nameEventSearch, cityEventSearch, fromDateSearch, toDateSearch, categorySearch);
+});
+// END categories for search
+function delActiveColor(className) {
+	let arrActiveColor = document.querySelectorAll("." + className);
+	// console.log("arr", arrActiveColor)
+	arrActiveColor.forEach(function(el){
+		el.classList.remove(className);
+		// console.log("delete class")
+	})
+};
 function sliceText(text, count) {
 	let sliced = text.replace(/<\/?[^>]+>/g,'');
 	sliced = sliced.slice(0,count);
@@ -47,7 +210,7 @@ let $btn = $('.datepicker_btn'),
 		
 		onSelect: function (dateText, inst) {
 		   console.log(dateText)
-		   datepickerStart = dateText;
+		   splitSearchDate(dateText);
 		},
 		minDate: new Date()
 	}).data('datepicker');
@@ -57,89 +220,31 @@ let $btn = $('.datepicker_btn'),
 	  dp.clear();
   	  $input.focus();
   });
+  function splitSearchDate(dates) {
+	delActiveColor("bg_chosen_day");
+	if (dates.indexOf(' - ') > -1) {
+		let dateArr = dates.split(' - ');
+		fromDateSearch = dateArr[0];
+		toDateSearch = dateArr[1];
+		
+		} else {
+			fromDateSearch = dates;
+		}
+};
 
-
-  var searchName = document.querySelector("#event_name");
-  var searchLocation = document.querySelector("#city");
-  var modalNotFound = document.querySelector(".container_modal_notfound");
-  var closeModalNotfound = document.querySelector(".close_modal_notfound");
-  var datepickerStart = "";
-  var datepickerEnd = "";
-  getCities("city");
-  
-  closeModalNotfound.addEventListener("click", function() {
-	  modalNotFound.style.display = "none";
-	  document.location.href = "/";
-	});
-  
-  let btnSearch = document.querySelector(".btn_search");
-  btnSearch.addEventListener('click',() => searchEvent("event_name", "city", datepickerStart, datepickerEnd));
-  
-  function viewEvent(id) {
-	  console.log("ID", id);
-	  console.log(window.location.href);
-	  let urlEvent = window.location.href + "event-page.html?id=" + id;
-	  window.open(urlEvent);
-  }
-  
-  function createEventCard(objItem) {
-	  let eventCardElements = 
-	  ` <div class="event_card_content">
-	  <div class="block_event_img">
-		  <img src="https://eventafisha.com/storage/` + objItem.images + `" alt="` + sliceText(objItem.title, 20) + `" class="event_card_img">
-	  </div>
-  
-	  <div class="event_card_title">` + sliceText(objItem.title, 60) + `</div>
-	  <div class="event_card_desc">` + sliceText(objItem.desc, 120) + `</div>
-	  <div class="card_icon_title">
-		  <svg xmlns="http://www.w3.org/2000/svg" class="event_card_icon" viewBox="0 0 24 24"><path d="M17 3v-2c0-.552.447-1 1-1s1 .448 1 1v2c0 .552-.447 1-1 1s-1-.448-1-1zm-12 1c.553 0 1-.448 1-1v-2c0-.552-.447-1-1-1-.553 0-1 .448-1 1v2c0 .552.447 1 1 1zm13 13v-3h-1v4h3v-1h-2zm-5 .5c0 2.481 2.019 4.5 4.5 4.5s4.5-2.019 4.5-4.5-2.019-4.5-4.5-4.5-4.5 2.019-4.5 4.5zm11 0c0 3.59-2.91 6.5-6.5 6.5s-6.5-2.91-6.5-6.5 2.91-6.5 6.5-6.5 6.5 2.91 6.5 6.5zm-14.237 3.5h-7.763v-13h19v1.763c.727.33 1.399.757 2 1.268v-9.031h-3v1c0 1.316-1.278 2.339-2.658 1.894-.831-.268-1.342-1.111-1.342-1.984v-.91h-9v1c0 1.316-1.278 2.339-2.658 1.894-.831-.268-1.342-1.111-1.342-1.984v-.91h-3v21h11.031c-.511-.601-.938-1.273-1.268-2z"/></svg>
-		  <div class="card_title">` + new Date(objItem.start_date).toLocaleDateString() + `</div>
-	  </div>
-	  <div class="card_icon_title">
-		  <svg xmlns="http://www.w3.org/2000/svg" class="event_card_icon" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
-		  <div class="card_title">` + objItem.address + `</div>
-	  </div>
-	  <div class="event_card_cat">` + objItem.category.title + `</div>
-	  <input class="btn_buy_ticket" type="submit" value="Купить билет" onclick="viewEvent(`+ objItem.id +`)">
-	</div>`;
-	  // console.log(test);
-  
-	  let listEventsElement = document.querySelector(".container_events");
-	  let eventCardElement = document.createElement('li');
-	  eventCardElement.className = "event_card";
-	  eventCardElement.innerHTML = eventCardElements;
-	  listEventsElement.append(eventCardElement);
-  }
-  
-  function getCities(elementSelect) {
-	  axios.get('https://eventafisha.com/api/v1/cities')
-	  .then(function (response) {
-		for(let item in response.data) {
-		  addOptionSelect(response.data[item], elementSelect);
-		};
-	  })
-	  .catch(function (error) {
-		// handle error
-		console.log(error);
-	  })
-	  .then(function () {
-		// always executed
-	  });
-  };
-  function addOptionSelect(item, elementSelect) {
-	  let selectCategory = document.getElementById(elementSelect);
-	  let option = document.createElement("option");
-	  option.value = item.id;
-	  option.innerHTML = item.title;
-	  selectCategory.add(option);
-	}
+// search function
+var fromDateSearch = '';
+var toDateSearch = '';
+var nameEventSearch = '';
+var cityEventSearch = '';
+var categorySearch = '';
   
   // для поиска по названию/городу
   function searchEvent(titleEl, cityEl, dateStart, dateEnd) {
-	  let nameEvent = document.getElementById(titleEl).value;
-	  let cityEvent = document.getElementById(cityEl).value;
-	  // searchRequest(nameEvent, cityEvent);
-	  paginationAjax('#pagination', nameEvent, cityEvent, dateStart, dateEnd, "");
+	nameEventSearch = document.getElementById(titleEl).value;
+	cityEventSearch = document.getElementById(cityEl).value;
+	// searchRequest(nameEvent, cityEvent);
+	paginationAjax('#pagination', nameEventSearch, cityEventSearch, fromDateSearch, toDateSearch, categorySearch);
   }
   function removeEventList() {
 	  var listEventEl = document.querySelector(".container_events");
@@ -183,7 +288,7 @@ let $btn = $('.datepicker_btn'),
 		  console.log("test", dataSource.total)
 		  return dataSource.total;
 	  },
-		pageSize: 20,
+		pageSize: 24,
 		showPageNumbers: true,
 		showPrevious: true,
 		showNext: true,
@@ -213,10 +318,8 @@ let $btn = $('.datepicker_btn'),
 			  });
 			  window.scrollTo(0, 0);
 		  }
-		  searchName.value = "";
-		  searchLocation.value = "";
-		  datepickerStart = "";
-		  datepickerEnd = "";
+		//   searchName.value = "";
+		//   searchLocation.value = "";
 		  
 		  // if (window.matchMedia("(max-width: 768px)").matches){
 		  // 	hideSearch();
@@ -228,11 +331,11 @@ let $btn = $('.datepicker_btn'),
 	};
   
 
-  // Open the dropdown window
+  // Open the dropdown window CATEGORY
   document.querySelector(".dropbtn").addEventListener("click", function() {
   	document.querySelector(".dropdown_content").classList.toggle("show");
     });
-  // Close the dropdown if the user clicks outside of it
+  // Close the dropdown if the user clicks outside of it CATEGORY
   window.onclick = function(event) {
 	if (!event.target.matches('.dropbtn')) {
 	  var dropdown = document.querySelector(".dropdown_content");
